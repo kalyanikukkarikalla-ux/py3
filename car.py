06c15995740d9a70b416656981f2f01307bbf635
Kukkarikalla kalyani <kalyanikukkarikalla@gmail.com>
2026-08-06 10:36:41 +0530

    Add files via upload

---
+++ car.py
@@ -0,0 +1,15 @@
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def __str__(self):
+        return f"{self.year} {self.brand} {self.model}"
+
+
+# Creating object
+car1 = Car("Toyota", "Fortuner", 2024)
+
+# Calling __str__ automatically
+print(car1)
